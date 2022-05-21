d6cce9e2120644a3e14163124dc43a72ad5dc811
abitwitch <arrakis256@gmail.com>
2022-05-21 10:21:47 -0400

    Update intoYTMusic.js

--- intoYTMusic.js
+++ intoYTMusic.js
@@ -1,17 +1,24 @@
-song="personal by stars"
+fullSongTitle="personal by stars"
 targetPlaylist="Test"
 
+//Load URL
+//TODO: await load page
+//TODO: Run script after load
+window.location.href = 'https://music.youtube.com/search?q='+encodeURIComponent(fullSongTitle);
+await sleep(2000);
 
+//Def sleep function
 function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
 
-
+//Click on menu for First Result
 firstResultButton=document.querySelector("#contents > ytmusic-responsive-list-item-renderer > ytmusic-menu-renderer > tp-yt-paper-icon-button")
 firstResultButton.click()
 
 await sleep(200);
 
+//Find and click "Add to playlist" button
 songMenu=document.querySelector("#items").childNodes
 for (let i = 0; i < songMenu.length; i++) {
   try{
@@ -24,8 +31,8 @@ for (let i = 0; i < songMenu.length; i++) {
 
 await sleep(200);
 
+//Find and click target playlist
 playlists=document.querySelector("#playlists").childNodes;
-
 for (let i = 0; i < playlists.length; i++) {
   playlistName=playlists[i].querySelector("#title").innerText;
   if (playlistName==targetPlaylist) {
